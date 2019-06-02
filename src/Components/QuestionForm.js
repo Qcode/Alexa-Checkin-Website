b87266ee25ee394efb483155068f64c96d38182d
michelle-t5 <michelle.teplitski@gmail.com>
2019-06-02 09:24:47 -0400

    spacing and font changes

--- src/Components/QuestionForm.js
+++ src/Components/QuestionForm.js
@@ -5,6 +5,8 @@ import AddButton from './AddButton';
 import QuestionCard from './QuestionCard';
 import NewQuestionCard from './NewQuestionCard';
 
+import './QuestionForm.css'
+
 class QuestionForm extends React.Component {
   constructor(props) {
     super(props);
@@ -30,7 +32,7 @@ class QuestionForm extends React.Component {
           setQuestion={this.props.setQuestion}
         />
         <AddButton handleModal={this.handleModal} />
-        <h1>Active Questions</h1>
+        <h1 className="activeQuestions">Active Questions</h1>
         {this.props.questions.map((question, index) => (
           <QuestionCard
             editQuestion={this.props.editQuestion}
--- src/Components/ResponseHistory.js
+++ src/Components/ResponseHistory.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './ResponseHistory.css';
 
 function ResponseHistory(props) {
   return (
     <div className="container">
-      <h3>{props.history.location.state.question}</h3>
+      <table>
+        <h3>{props.history.location.state.question}</h3>
+      </table>
       {props.history.location.state.responses.map((response, index) => (
-        <p>
-          <b>{`May ${index + 1}:`}</b>
-          {` ${response}`}
-        </p>
+        <tr>
+          <td><b>{`May ${index + 1}:`}</b></td>
+          <td>{` ${response}`}</td>
+        </tr>
       ))}
     </div>
   );
